6e3e9335b9d898081b30239a6b410ea217f228da
rajjaiswal07 <rajjaiswal07.rj@gmail.com>
2020-11-16 13:19:50 +0530

    Menu Bar developed

--- app/src/main/java/com/example/kissan_setu/MainActivity.java
+++ app/src/main/java/com/example/kissan_setu/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.kissan_setu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.kissan_setu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile:
+                Toast.makeText(this, "Profile is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.settings:
+                Toast.makeText(this, "Settings is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.contact_us:
+                Toast.makeText(this, "Contact Us is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.products:
+                Toast.makeText(this, "Products is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.vegetables:
+                Toast.makeText(this, "Vegetables is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.fruits:
+                Toast.makeText(this, "Fruits is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.spices:
+                Toast.makeText(this, "Spices is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.grains:
+                Toast.makeText(this, "Grains is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.locations:
+                Toast.makeText(this, "Locations is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.status:
+                Toast.makeText(this, "Status is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.about_us:
+                Toast.makeText(this, "About Us is selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
